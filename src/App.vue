9f83e3692827b80e3a96b64be9dbb7036837a5ed
Cameron Moses <cameron.moses@fiig.com.au>
2021-09-16 09:52:41 +1000

    Change project name

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
+      <div class="container">
         <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <button
+            type="button"
+            class="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar"
+          >
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Project name</a>
+          </button>
+          <router-link class="navbar-brand" to="/">My Vue App</router-link>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li class="active"><router-link to="/">Home</router-link></li>
+          <ul class="nav navbar-nav">
             <li><router-link to="users">Users</router-link></li>
-            
-        </ul>
-        </div><!--/.nav-collapse -->
-    </div>
-</nav>
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
+      </div>
+    </nav>
     <div class="container">
       <router-view></router-view>
     </div>
@@ -27,14 +33,14 @@
 </template>
 
 <script>
-import Alert from './components/Alert'
+import Alert from "./components/Alert";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Alert
-  }
-}
+    Alert,
+  },
+};
 </script>
 
 <style>
